9be82a02895a8953287d0657ced5239894639d35
Johan Medrano <medrano@etud.insa-toulouse.fr>
2019-01-23 16:30:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pip install twine
-
 import io
 import os
 from setuptools import find_packages, setup
 
-# Package meta-data.
 NAME = 'fakehome'
 DESCRIPTION = ''
 URL = 'https://github.com/yop0/fakehome'
@@ -17,29 +12,18 @@ AUTHOR = 'Johan Medrano'
 REQUIRES_PYTHON = '>=3.6.0'
 VERSION = '0.1.0'
 
-# What packages are required for this module to be executed?
 REQUIRED = [
     'owlready2', 'tqdm', 'wget'
 ]
 
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier
-# for that!
-
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
     with open(os.path.join(here, NAME, '__version__.py')) as f:
@@ -47,8 +31,6 @@ if not VERSION:
 else:
     about['__version__'] = VERSION
 
-
-# Where the magic happens:
 setup(
     name=NAME,
     version=about['__version__'],
